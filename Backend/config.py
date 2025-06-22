9a4362cbbf8bacaf8b9d71eb3bb58af7705e1cf3
Deb <Deb@mac.myfiosgateway.com>
2025-06-22 00:02:03 -0700

    chore: add config.py for deployment

---
+++ Backend/config.py
@@ -0,0 +1,5 @@
+GEMINI_API_KEY = "AIzaSyABKNYyhWzk98T9z-UzqAZhuoxbWG6qGJc"
+MONGO_URI = "mongodb+srv://deb458746:69IR1GZDHpDCJUUF@cluster0.pfnawqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
